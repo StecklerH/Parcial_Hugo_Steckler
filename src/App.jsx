c8c3a71536083d2f06f9623112d64e249bb371a0
StecklerH <steckler.1@hotmail.com>
2023-05-30 19:55:03 -0300

    segundo commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Cart from './Components/Cart'
+
+function App() {
+
+
+  return (
+    <div>
+      <h1>Carga de estudiantes</h1>
+      <form></form>
+      <Cart/>
+    </div>
+  )
+}
+
+export default App
